036fce66fb8e7e9d1caa5c1ce71c19f352fb000f
Weixian <43234101+weixian-zhang@users.noreply.github.com>
2023-03-09 17:15:21 +0800

    feat: Added py script with comments as requirements to automate creation of ip group and firewall network rule

---
+++ create-ipgroup-firewall-rules.py
@@ -0,0 +1,8 @@
+
+# get session host user assignment with VM private IP from resource graph
+
+# for each record in RGQuery, create IP Group if not exist, IP Group name will be username without "@...com"
+
+# create IP group for each on-premise subnets
+
+# with a pre-formatted user <-> onprem-subnet mapping list, read the list and create network rule accordingly 
